2dca292ceb5bdb7b22b6edc2df4278d801b79801
gTopa <gtopa@stuy.edu>
2016-09-15 19:04:04 -0400

    think i've finished

--- occupationRandomizer.py
+++ occupationRandomizer.py
@@ -1,3 +1,5 @@
+import random
+
 f = open('occupations.csv', 'r')
 s=f.read()
 f.close()
@@ -5,15 +7,18 @@ s=s.split("\n")
 s=s[1:len(s)-3]
 d={}
 for occupation in s:
-    if occupation[0:1]== '"':
-        occupation=occupation.split(',')
+    occupation=occupation.split(',')
+    if occupation[0][0:1]== '"':
         while len(occupation)>2:
             occupation[0]=occupation[0]+','+occupation[1]
             occupation.pop(1)
-    else:
-        occupation=occupation.split(',')
     d[occupation[0]]=float(occupation[1])
-print d
 
 def pickRandom():
-    
+    listOfJobs=d.keys()
+    weightedList=[]
+    for jobs in listOfJobs:
+        weightedList.extend([jobs]*int((d[jobs]*10)))
+    return random.choice(weightedList)
+
+print pickRandom()
